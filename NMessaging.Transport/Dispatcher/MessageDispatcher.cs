2379d35e4a79badf4491ae264a56f2a30030c9f6
Marco Encarnacao <marco.encarnacao@wonga.com>
2012-09-12 22:03:26 +0100

    Started adding a dispatcher worker class to handle the messages added to the "messages to be sent" queue

--- NMessaging.Transport/Dispatcher/MessageDispatcher.cs
+++ NMessaging.Transport/Dispatcher/MessageDispatcher.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Linq;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using NMessaging.Transport.Message.Data;
 
 namespace NMessaging.Transport.Dispatcher
@@ -11,6 +16,7 @@ namespace NMessaging.Transport.Dispatcher
 
         private static ConcurrentQueue<MessageToSendOnQueue> MessagesToSendOnQueue = default(ConcurrentQueue<MessageToSendOnQueue>);
         private MessageDispatcherSettings _oMessageDispatcherSettings = default(MessageDispatcherSettings);
+        private readonly Lazy<IList<Thread>> _oWorkers = new Lazy<IList<Thread>>() ;
 
 
         //////////////////////////////
@@ -20,6 +26,11 @@ namespace NMessaging.Transport.Dispatcher
         public MessageDispatcher(MessageDispatcherSettings pMessageDispatcherSettings)
         {
             _oMessageDispatcherSettings = pMessageDispatcherSettings;
+
+            for (var iCoreNumber = 0; iCoreNumber < Environment.ProcessorCount; iCoreNumber++)
+            {
+                _oWorkers.Value.Add(new Thread(delegate() { }));
+            }
         }
 
 
@@ -29,7 +40,8 @@ namespace NMessaging.Transport.Dispatcher
 
         public void Send(MessageDataToSend pMessageToSend, MessageDataToSendSettings pMessageDataToSendSettings)
         {
-            MessagesToSendOnQueue.Enqueue(new MessageToSendOnQueue(pMessageToSend, pMessageDataToSendSettings));
+            Task.Factory.StartNew(() => MessagesToSendOnQueue.Enqueue(new MessageToSendOnQueue(pMessageToSend,
+                                                                                               pMessageDataToSendSettings)));
         }
 
         //////////////////////////////
---
+++ NMessaging.Transport/Dispatcher/MessageDispatcherWorker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+
+namespace NMessaging.Transport.Dispatcher
+{
+    public class MessageDispatcherWorker
+    {
+        //////////////////////////////
+        //          MEMBERS         //
+        //////////////////////////////
+
+        private readonly ConcurrentQueue<MessageToSendOnQueue> _oMessagesToSendOnQueue = default(ConcurrentQueue<MessageToSendOnQueue>);
+        private MessageDispatcher _oMessageDispatcher = default(MessageDispatcher);
+        private readonly AutoResetEvent _oAutoResetEvent = new AutoResetEvent(true);
+
+
+        //////////////////////////////
+        //        CONSTRUCTORS      //
+        //////////////////////////////
+
+        public MessageDispatcherWorker(MessageDispatcher pMessageDispatcher, ConcurrentQueue<MessageToSendOnQueue> pMessageToSendOnQueues)
+        {
+            _oMessageDispatcher = pMessageDispatcher;
+            _oMessagesToSendOnQueue = pMessageToSendOnQueues;
+        }
+
+
+        //////////////////////////////
+        //          METHODS         //
+        //////////////////////////////
+
+        private void DoWork()
+        {
+            while (true)
+            {
+                try
+                {
+                    MessageToSendOnQueue oMessageToSendOnQueue = null;
+
+                    while (true)
+                    {
+                        _oMessagesToSendOnQueue.TryDequeue(out oMessageToSendOnQueue);
+
+                        if (oMessageToSendOnQueue != null)
+                        {
+                            break;
+                        }
+
+                        _oAutoResetEvent.WaitOne();
+                    }
+
+                    this.SendMessage(oMessageToSendOnQueue);
+                }
+                catch (Exception)
+                {
+                    //Restart the thread?
+                }
+            }
+        }
+
+        //////////////////////////////
+
+        public void NotifyOfNewMessageOnQueue()
+        {
+            _oAutoResetEvent.Set();
+        }
+
+        //////////////////////////////
+
+        private void SendMessage(MessageToSendOnQueue pMessageToSendOnQueue)
+        {
+
+        }
+
+        //////////////////////////////
+    }
+}
